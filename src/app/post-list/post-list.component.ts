9af9683bceb24e0ecffac5f6bc813a04b3cfd932
Mathias Böhm <mathias.boehm@struktuhr.de>
2020-02-09 11:27:37 +0100

    finish migration

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator, MatSort } from '@angular/material';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { merge } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Post } from '../shared/post';
